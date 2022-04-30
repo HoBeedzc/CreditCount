b10fbfd9f04668f5a9b8ea65a2044a6df44afa68
Hobee-jcserver <18377240@buaa.edu.cn>
2022-05-01 00:38:23 +0800

    react 重构menu

---
+++ static/menubar/menu.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const {Menu, Input} = semanticUIReact;
+
+class MenuBar extends React.Component {
+    state = {activeItem: 'home'}
+
+    handleItemClick = (e, {name}) => this.setState({activeItem: name})
+
+    render() {
+
+        const divStyle = {
+            'z-index': '100',
+            'background-color': '#13227a',
+            'color': '#FFF',
+        }
+
+        const aStyle = {
+            'color': '#FFF',
+        }
+
+        const {activeItem} = this.state
+
+        return (
+            <Menu className="fixed borderless" style={divStyle} secondary>
+                <Menu.Item
+                    name=' CreditCount by Hobee '
+                    style={aStyle}
+                    active={activeItem === 'CreditCount by Hobee'}
+                    onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                    name=' BUAA SEM 本科一般专业课学分核算 '
+                    style={aStyle}
+                    active={activeItem === 'BUAA SEM 本科一般专业课学分核算'}
+                    onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                    name=' 愿大家都能顺利毕业 '
+                    style={aStyle}
+                    active={activeItem === ' 愿大家都能顺利毕业 '}
+                    onClick={this.handleItemClick}
+                />
+                <Menu.Menu position='right'>
+                    <Menu.Item>
+                        <Input icon='search' placeholder='Search...'/>
+                    </Menu.Item>
+                    <Menu.Item
+                        name='logout'
+                        active={activeItem === 'logout'}
+                        onClick={this.handleItemClick}
+                    />
+                </Menu.Menu>
+            </Menu>
+        );
+    }
+}
+
+ReactDOM.render(
+    <MenuBar/>,
+    document.getElementById('menu')
+);
