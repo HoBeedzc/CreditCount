5d969f1c1613171d220f9d409e55a64915aa4727
hobee_mac <lzqpublic@163.com>
2022-08-21 18:04:58 +0800

    更新日志增加翻页功能

--- static/sidebar/updateLog.js
+++ static/sidebar/updateLog.js
@@ -34,6 +34,137 @@ class UpdateLog extends React.Component {
         super(props);
         this.state = {
             lastUpdateDate: '2020-11-11',
+            curPage: 1,
+            maxPage: 1,
+            showPageNum: [1],
+        }
+    }
+
+    handleClickPervBtn() {
+        if (this.state.curPage > 1) {
+            this.setState({
+                curPage: this.state.curPage - 1,
+            });
+        } else {
+            return
+        }
+
+        if (this.state.curPage > 2) {
+            let showPageTemp;
+            switch (this.state.maxPage) {
+                case 1:
+                    showPageTemp = [1];
+                    break;
+                case 2:
+                    showPageTemp = [1, 2];
+                    break;
+                default:
+                    showPageTemp = [this.state.curPage - 2, this.state.curPage - 1, this.state.curPage];
+            }
+            this.setState({
+                showPageNum: showPageTemp.slice(0, showPageTemp.length),
+            })
+        } else {
+            let showPageTemp;
+            switch (this.state.maxPage) {
+                case 1:
+                    showPageTemp = [1];
+                    break;
+                case 2:
+                    showPageTemp = [1, 2];
+                    break;
+                default:
+                    showPageTemp = [1, 2, 3];
+            }
+            this.setState({
+                showPageNum: showPageTemp.slice(0, showPageTemp.length),
+            })
+        }
+    }
+
+    handleClickNextBtn() {
+        if (this.state.curPage < this.state.maxPage) {
+            this.setState({
+                curPage: this.state.curPage + 1,
+            })
+        } else {
+            return
+        }
+
+        if (this.state.curPage < this.state.maxPage - 1) {
+            let showPageTemp;
+            switch (this.state.maxPage) {
+                case 1:
+                    showPageTemp = [1];
+                    break;
+                case 2:
+                    showPageTemp = [1, 2];
+                    break;
+                default:
+                    showPageTemp = [this.state.curPage, this.state.curPage + 1, this.state.curPage + 2];
+            }
+            this.setState({
+                showPageNum: showPageTemp.slice(0, showPageTemp.length),
+            })
+        } else {
+            let showPageTemp;
+            switch (this.state.maxPage) {
+                case 1:
+                    showPageTemp = [1];
+                    break;
+                case 2:
+                    showPageTemp = [1, 2];
+                    break;
+                default:
+                    showPageTemp = [this.state.maxPage - 2, this.state.maxPage - 1, this.state.maxPage];
+            }
+            this.setState({
+                showPageNum: showPageTemp.slice(0, showPageTemp.length),
+            })
+        }
+    }
+
+    handleClickPageBtn(page) {
+        if (page !== this.state.curPage) {
+            this.setState({
+                curPage: page,
+            });
+        }
+
+        if (page === 1) {
+            let showPageTemp;
+            switch (this.state.maxPage) {
+                case 1:
+                    showPageTemp = [1];
+                    break;
+                case 2:
+                    showPageTemp = [1, 2];
+                    break;
+                default:
+                    showPageTemp = [1, 2, 3];
+            }
+            this.setState({
+                showPageNum: showPageTemp.slice(0, showPageTemp.length),
+            });
+        } else if (page === this.state.maxPage) {
+            let showPageTemp;
+            switch (this.state.maxPage) {
+                case 1:
+                    showPageTemp = [1];
+                    break;
+                case 2:
+                    showPageTemp = [1, 2];
+                    break;
+                default:
+                    showPageTemp = [page - 2, page - 1, page];
+            }
+            this.setState({
+                showPageNum: showPageTemp.slice(0, showPageTemp.length),
+            });
+        } else {
+            this.setState({
+                showPageNum: [page - 1, page, page + 1],
+            });
         }
     }
 
@@ -50,20 +181,65 @@ class UpdateLog extends React.Component {
         let data = JSON.parse(localStorage.getItem('vi'));
 
         if (this.state.lastUpdateDate !== data[0]['date']) {
+
+            let maxPageTemp = data.length % 5 === 0 ? parseInt(data.length / 5) : parseInt(data.length / 5) + 1;
+            let showPageTemp;
+            switch (maxPageTemp) {
+                case 1:
+                    showPageTemp = [1];
+                    break;
+                case 2:
+                    showPageTemp = [1, 2];
+                    break;
+                default:
+                    showPageTemp = [1, 2, 3];
+            }
+
             this.setState({
                 lastUpdateDate: data[0]['date'],
+                maxPage: data.length % 5 === 0 ? parseInt(data.length / 5) : parseInt(data.length / 5) + 1,
+                showPageNum: showPageTemp.slice(0, showPageTemp.length),
             });
         }
 
-        let showData = data.slice(0, 5);
+        let showData = data.slice((this.state.curPage - 1) * 5, this.state.curPage * 5);
         const verInfo = showData.map((item, index) => {
             return <VersionInfo version={item['version']} info={item['info']}/>
         });
+
         return (
             <Table.Body>
                 {verInfo}
             </Table.Body>
         );
+    }
+
+    renderPageInfo() {
+
+        const pageInfo = this.state.showPageNum.map((item, index) => {
+            return <Menu.Item as='a' active={this.state.curPage === item}
+                              onClick={() => this.handleClickPageBtn(item)}>{item}</Menu.Item>
+        });
+
+        return (
+            <Table.Footer>
+                <Table.Row>
+                    <Table.HeaderCell colSpan='3'>
+                        <Menu floated='right' pagination>
+                            <Menu.Item as='a' icon disabled={this.state.curPage === 1}
+                                       onClick={() => this.handleClickPervBtn()}>
+                                <Icon name='chevron left'/>
+                            </Menu.Item>
+                            {pageInfo}
+                            <Menu.Item as='a' icon disabled={this.state.curPage === this.state.maxPage}
+                                       onClick={() => this.handleClickNextBtn()}>
+                                <Icon name='chevron right'/>
+                            </Menu.Item>
+                        </Menu>
+                    </Table.HeaderCell>
+                </Table.Row>
+            </Table.Footer>
+        )
     }
 
     render() {
@@ -82,23 +258,7 @@ class UpdateLog extends React.Component {
                         </Table.Row>
                     </Table.Header>
                     {this.renderVersionInfo()}
-                    <Table.Footer>
-                        <Table.Row>
-                            <Table.HeaderCell colSpan='3'>
-                                <Menu floated='right' pagination>
-                                    <Menu.Item as='a' icon>
-                                        <Icon name='chevron left'/>
-                                    </Menu.Item>
-                                    <Menu.Item as='a'>1</Menu.Item>
-                                    <Menu.Item as='a'>2</Menu.Item>
-                                    <Menu.Item as='a'>3</Menu.Item>
-                                    <Menu.Item as='a' icon>
-                                        <Icon name='chevron right'/>
-                                    </Menu.Item>
-                                </Menu>
-                            </Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Footer>
+                    {this.renderPageInfo()}
                 </Table>
             </Segment>
         );
